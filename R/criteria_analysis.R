a33d2ba0548cd5e31b26448b0e399418a952011d
alexchouraki <alexchouraki@me.com>
2017-11-03 17:50:32 +0100

    with criteria analysis function

---
+++ R/criteria_analysis.R
@@ -0,0 +1,17 @@
+#' criteria_analysis
+#'
+#' @param criteria the criteria we want to focus on, which should be among the columns of the Happiness dataset
+#' @param top the number of countries we want to list
+#' @param focusyear the year we want to focus on
+#' @return the top countries for one given criteria
+#' @import tidyverse
+#' @export
+#'
+#' @example
+#' criteria_analysis(Freedom, 10, 2016)
+criteria_analysis <- function(criteria, top, focusyear){
+    data_Happiness %>%
+    select(year == focusyear, Country, criteria) %>%
+    distinct() %>%
+    head(top)
+}
